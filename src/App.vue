cc02be19545f60c162d8ee429ce642d342130527
lazy11233 <1257625996@qq.com>
2018-09-04 02:16:11 +0800

    post component

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,20 @@
 <template>
   <div id="app">
-    <router-view/>
     <Header></Header>
+    <div class="main">
+    <PostList></PostList>
+
+    </div>
   </div>
 </template>
 <script>
 import Header from './components/Header.vue';
+import PostList from './components/PostList.vue';
 export default {
   name: 'App',
   components: {
     Header,
+    PostList
   }
 }
 </script>
@@ -17,5 +22,12 @@ export default {
 * {
   padding: 0;
   margin: 0;
+}
+ul,li {
+  list-style: none;
+}
+.main {
+  width: 80%;
+  margin: 0 auto;
 }
 </style>
